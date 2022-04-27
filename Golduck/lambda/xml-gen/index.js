c9f452645a13522eacd9a9abb476887a8ef7d190
GecheleStefano <67332771+GecheleStefano@users.noreply.github.com>
2022-04-27 11:50:54 +0200

    utilizzo libreria xml-js

--- Golduck/lambda/xml-gen/index.js
+++ Golduck/lambda/xml-gen/index.js
@@ -1,40 +1,41 @@
-
-var fxp = require('fast-xml-parser');
 var fs = require('fs');
+const convert = require('xml-js');
 
-var parser = new fxp.XMLParser();
-var builder = new fxp.XMLBuilder({format: true});
+const maxTime = 2005;
 
-var maxTime = 2005;
+var data = fs.readFileSync('./test-result.xml', 'utf8');
 
-var data = fs.readFileSync( './test-result.xml');
-var xmlObj = parser.parse(data.toString('utf-8'));
+const options = {
+    compact: true,
+    ignoreComment: false,
+    spaces: 2
+};
+const jsonData = JSON.parse(convert.xml2json(data, options));
 
-xmlObj.ResultList.ClassResult.forEach((classRes, index, obj) => {
+jsonData.ResultList.ClassResult.forEach((classRes, index, obj) => {
 
     if (Array.isArray(classRes.PersonResult)) {
         obj[index].PersonResult
-        .filter(person => person.Result.Time == null || person.Result.Time > maxTime)
-        .forEach(toRemove => obj[index].PersonResult.splice(obj[index].PersonResult.indexOf(toRemove), 1));
+            .filter(person => person.Result.Time == null || person.Result.Time > maxTime)
+            .forEach(toRemove => obj[index].PersonResult.splice(obj[index].PersonResult.indexOf(toRemove), 1));
     } else {
-        if(classRes.PersonResult.Result.Time != null) {
-            if(classRes.PersonResult.Result.Time > maxTime) {
+        if (classRes.PersonResult.Result.Time != null) {
+            if (classRes.PersonResult.Result.Time > maxTime) {
                 delete obj[index].PersonResult;
             }
-        }else{
+        } else {
             delete obj[index].PersonResult;
         }
     }
 });
 
-var xmlStr = builder.build(xmlObj)
 
-fs.writeFile("filtred.xml", xmlStr, 'utf8', function (err) {
+var result = convert.json2xml(jsonData, options);
+
+fs.writeFile("filtred.xml", result, 'utf8', function (err) {
     if (err) {
         console.log("An error occured while writing XML String to File.");
         return console.log(err);
     }
- 
     console.log("XML file has been saved.");
 });
-
