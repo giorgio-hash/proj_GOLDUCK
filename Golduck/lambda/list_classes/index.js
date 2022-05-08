cb40cfcf447e5dc32ce0f338720ad0b7f882309a
GecheleStefano <67332771+GecheleStefano@users.noreply.github.com>
2022-05-08 10:34:48 +0200

    update dependency

--- Golduck/lambda/list_classes/index.js
+++ Golduck/lambda/list_classes/index.js
@@ -1,38 +1,33 @@
-
-
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-const fxp = require('fast-xml-parser');
-const parser = new fxp.XMLParser();
+const parser = require('xml-js');
 
 exports.handler = async (event) => {
-    try {
-        var params = {
-          Bucket: 'xmlres', 
-          Key: 'results-' + event.queryStringParameters.id + '.xml'
-        };
-        
-        var results = await s3.getObject(params, function(err, data) {
-          if (err) {
-            console.log('Error', err);
-          }
-          else {
-            return data;
-          }
-        }).promise();
-        
-        var xml = parser.parse(results.Body.toString('utf-8'));
-        var classList = xml.ResultList.ClassResult.map(classRes => {
-          return classRes.Class.Name;
-        });
-        
-        return sendRes(200, JSON.stringify(classList));
-    }
-    catch (error) {
-        console.log(error);
-        return sendRes(404, 'Error');
-    }
+  try {
+    var params = {
+      Bucket: 'xmlres',
+      Key: 'results-' + event.queryStringParameters.id + '.xml'
+    };
+
+    var results = await s3.getObject(params, function (err, data) {
+      if (err) {
+        console.log('Error', err);
+      } else {
+        return data;
+      }
+    }).promise();
+
+    var xml = JSON.parse(parser.xml2json(results.Body.toString('utf-8')));
+    var classList = xml.ResultList.ClassResult.map(classRes => {
+      return classRes.Class.Name;
+    });
+
+    return sendRes(200, JSON.stringify(classList));
+  } catch (error) {
+    console.log(error);
+    return sendRes(404, 'Error');
+  }
 };
 
 /*
@@ -40,20 +35,17 @@ ClassResult.Class.Name
 */
 
 const sendRes = (status, body) => {
-    var response = {
-        statusCode: status,
-        headers: {
-            "Content-Type" : "application/json",
-            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-            "Access-Control-Allow-Methods" : "OPTIONS,POST,PUT",
-            "Access-Control-Allow-Credentials" : true,
-            "Access-Control-Allow-Origin" : "*",
-            "X-Requested-With" : "*"
-        },
-        body: body
-    };
-    return response;
+  var response = {
+    statusCode: status,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,PUT",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": "*",
+      "X-Requested-With": "*"
+    },
+    body: body
+  };
+  return response;
 };
-
-
-
