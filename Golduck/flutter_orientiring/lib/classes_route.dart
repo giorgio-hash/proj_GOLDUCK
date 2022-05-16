8154c75ba279a16893cad6a79be94de6732b9c60
GecheleStefano <67332771+GecheleStefano@users.noreply.github.com>
2022-05-16 21:58:41 +0200

    Revert "delete flutter app"
    This reverts commit 6e93d744734df7bb8dbe464ea3d6301d8afda772.

---
+++ Golduck/flutter_orientiring/lib/classes_route.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import './globals.dart';
+import 'classifiche_route.dart';
+
+Future<List<String>> fetchClasses(String raceid) async {
+  final response = await http.get(Uri.parse('$apiUrl/list_classes?id=$raceid'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return List<String>.from(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load classes');
+  }
+}
+
+class ClassesRoute extends StatefulWidget {
+  final String raceid;
+  const ClassesRoute(this.raceid, {Key? key}) : super(key: key);
+
+  @override
+  _ClassesRouteState createState() => _ClassesRouteState();
+}
+
+class _ClassesRouteState extends State<ClassesRoute> {
+  late Future<List<String>> futureClasses;
+
+  @override
+  void initState() {
+    super.initState();
+    futureClasses = fetchClasses(widget.raceid);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Classes'),
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.refresh),
+              tooltip: 'Refresh',
+              onPressed: () {
+                _refreshData();
+              }),
+        ],
+      ),
+      body: Center(
+        child: FutureBuilder<List<String>>(
+          future: futureClasses,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<String> classes = snapshot.data!;
+              return RefreshIndicator(
+                onRefresh: _refreshData,
+                child: ListView.builder(
+                  itemCount: classes.length,
+                  itemBuilder: ((context, index) => ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ClassificheRoute(
+                                  widget.raceid, classes[index]),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(classes[index]),
+                        ),
+                      )),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<void> _refreshData() async {
+    setState(() {
+      futureClasses = fetchClasses(widget.raceid);
+    });
+  }
+}
