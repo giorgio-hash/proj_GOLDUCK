a1b960f17a953580944472ceb6544b5448dfd5a5
Davide <lilflowz99@gmail.com>
2022-05-16 13:26:30 +0200

    Add refresh gesture

--- Golduck/flutter/Orieentering/lib/start_route.dart
+++ Golduck/flutter/Orieentering/lib/start_route.dart
@@ -8,7 +8,8 @@ import 'package:intl/intl.dart';
 import './globals.dart';
 
 Future<List<dynamic>> fetchStart(String raceid, String clazz) async {
-  final response = await http.get(Uri.parse('$apiUrl/list_start?id=$raceid&class=$clazz'));
+  final response =
+      await http.get(Uri.parse('$apiUrl/list_start?id=$raceid&class=$clazz'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
@@ -23,7 +24,8 @@ Future<List<dynamic>> fetchStart(String raceid, String clazz) async {
 }
 
 class StartRoute extends StatefulWidget {
-  final String raceid,clazz;
+  final String raceid, clazz;
+
   const StartRoute(this.raceid, this.clazz, {Key? key}) : super(key: key);
 
   @override
@@ -39,35 +41,43 @@ class _StartRouteState extends State<StartRoute> {
     futureStarts = fetchStart(widget.raceid, widget.clazz);
   }
 
+  Future<Null> _refresh() async {
+    futureStarts = fetchStart(widget.raceid, widget.clazz);
+    //Redraw widgets
+    setState(() => {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Start'),
       ),
-      body: Center(
-        child: FutureBuilder<List<dynamic>>(
-          future: futureStarts,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List starts = snapshot.data!;
-              return ListView.builder(
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        child: Center(
+          child: FutureBuilder<List<dynamic>>(
+            future: futureStarts,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List starts = snapshot.data!;
+                return ListView.builder(
                   itemCount: starts.length,
                   itemBuilder: (_, index) => Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
                     padding: const EdgeInsets.all(20.0),
                     decoration: BoxDecoration(
-                      color: const Color(0xff4f92da),
-                      borderRadius: BorderRadius.circular(15.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.blue.withOpacity(0.15),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: Offset(0, 3),
-                        ),
-                      ]
-                    ),
+                        color: const Color(0xff4f92da),
+                        borderRadius: BorderRadius.circular(15.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.blue.withOpacity(0.15),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ]),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -80,44 +90,42 @@ class _StartRouteState extends State<StartRoute> {
                           ),
                         ),
                         const SizedBox(height: 10),
-
                         Text(
-                            "Organisation: ${snapshot.data![index]["org"]}",
-                            style: const TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w500,
-                            ),
+                          "Organisation: ${snapshot.data![index]["org"]}",
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.fromLTRB(0, 5.0, 0, 5.0),
-                          child: Builder(
-                            builder: (context) {
-                              //Transform ISO date to more readable
-                              DateTime date = DateTime.parse(snapshot.data![index]["time"]);
-                              DateFormat dateFormat = DateFormat("HH:mm:ss");
-                              return Text(
-                                "Time: ${dateFormat.format(date)}",
-                                style: const TextStyle(
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.w500,
-                                    decoration: TextDecoration.underline,
-
-                                ),
-                              );
-                            }
-                          ),
+                          child: Builder(builder: (context) {
+                            //Transform ISO date to more readable
+                            DateTime date =
+                                DateTime.parse(snapshot.data![index]["time"]);
+                            DateFormat dateFormat = DateFormat("HH:mm:ss");
+                            return Text(
+                              "Time: ${dateFormat.format(date)}",
+                              style: const TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w500,
+                                decoration: TextDecoration.underline,
+                              ),
+                            );
+                          }),
                         ),
                       ],
                     ),
                   ),
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
 
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-          },
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
         ),
       ),
     );
