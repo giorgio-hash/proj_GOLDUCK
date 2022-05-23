09702485a3e2fcfb0bf6a867d34426b5b5540015
Davide <lilflowz99@gmail.com>
2022-05-23 18:36:41 +0200

    Update Menu

--- Golduck/flutter_orientiring/lib/menu.dart
+++ Golduck/flutter_orientiring/lib/menu.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'classes_route.dart';
 
+
 class MenuRoute extends StatelessWidget {
   final String raceid;
 
@@ -11,30 +12,55 @@ class MenuRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Available races'),
+          title: const Text('Choose an option'),
         ),
-        body: Column(children: <Widget>[
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ClassesRoute(raceid, "StartList"),
-                ),
-              );
-            },
-            child: const Text("StartList"),
+        body: Container(
+          padding: const EdgeInsets.fromLTRB(5, 15, 5, 15),
+          child: Stack(
+            children: [
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Are you looking For StartList or Live Results?',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                      ),),
+                  ]
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 50, 0, 50),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ClassesRoute(raceid, "StartList"),
+                            ),
+                          );
+                        },
+                        child: const Text("StartList"),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ClassesRoute(raceid, "Results"),
+                              ),
+                            );
+                          },
+                          child: const Text("Results"))
+                    ]),
+              )
+            ],
           ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ClassesRoute(raceid, "Results"),
-                  ),
-                );
-              },
-              child: Text("Results"))
-        ]));
+        )
+
+    );
   }
 }
