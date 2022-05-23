9930f7e7d4cb50853ecc4d219fc0a3ac5aa8811e
fassolari1 <fabioassolari1@gmail.com>
2022-05-23 18:16:06 +0200

    Drawer

--- Golduck/flutter_orientiring/lib/classes_route.dart
+++ Golduck/flutter_orientiring/lib/classes_route.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import './globals.dart';
+import 'about_us.dart';
 import 'classifiche_route.dart';
 import 'start_route.dart';
 
@@ -55,6 +56,7 @@ class _ClassesRouteState extends State<ClassesRoute> {
               }),
         ],
       ),
+      drawer: const NavigationDrawer(),
       body: Center(
         child: FutureBuilder<List<String>>(
           future: futureClasses,
@@ -108,4 +110,73 @@ class _ClassesRouteState extends State<ClassesRoute> {
       futureClasses = fetchClasses(widget.raceid);
     });
   }
+}
+
+
+// Drawer Class
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+        child: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildHeader(context),
+            buildMenuItems(context),
+          ],
+        )),
+      );
+
+  Widget buildHeader(BuildContext context) => Container(
+    color: Colors.blue.shade700,
+    padding: EdgeInsets.only(
+      top: 24 + MediaQuery.of(context).padding.top,
+      bottom: 24,
+    ),
+    child: Column(
+      children: const [
+        CircleAvatar(radius: 52,),
+        Text('Orienteering APP', style: TextStyle(fontSize: 28, color: Colors.white),),
+        Text('Races Results', style: TextStyle(fontSize: 15, color: Colors.white),)
+      ],
+    ),
+  );
+  
+  Widget buildMenuItems(BuildContext context) => Container(
+    padding: const EdgeInsets.all(24),
+    child: Wrap(
+      runSpacing: 16,
+      children: [
+        /* ListTile(
+          leading: const Icon(Icons.home_outlined),
+          title: const Text('Home'),
+          onTap: () {},
+        ), */
+        ListTile(
+          leading: const Icon(Icons.run_circle_outlined),
+          title: const Text('List Races'),
+          onTap: () {
+            Navigator.pop(context); // close the Draw
+            Navigator.pop(context); // return to menu
+            Navigator.pop(context); // return to home page
+          },
+        ),
+        const Divider(color: Colors.black54,),
+        ListTile(
+          leading: const Icon(Icons.people_alt_sharp),
+          title: const Text('About us'),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const about_us(),
+              ),
+            );
+          },
+        ),
+      ],
+    ),
+  );
 }
