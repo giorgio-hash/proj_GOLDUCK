6b86e78a88ed24bd769a471b854077aa4fc8cf4a
fassolari1 <fabioassolari1@gmail.com>
2022-05-23 18:33:46 +0200

    Revert "Drawer"
    This reverts commit 9930f7e7d4cb50853ecc4d219fc0a3ac5aa8811e.

--- Golduck/flutter_orientiring/lib/menu.dart
+++ Golduck/flutter_orientiring/lib/menu.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'about_us.dart';
 import 'classes_route.dart';
 
 class MenuRoute extends StatelessWidget {
@@ -14,7 +13,6 @@ class MenuRoute extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Available races'),
         ),
-        drawer: const NavigationDrawer(),
         body: Column(children: <Widget>[
           ElevatedButton(
             onPressed: () {
@@ -36,73 +34,7 @@ class MenuRoute extends StatelessWidget {
                   ),
                 );
               },
-              child: const Text("Results"))
+              child: Text("Results"))
         ]));
   }
-}
-// Drawer Class
-class NavigationDrawer extends StatelessWidget {
-  const NavigationDrawer({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => Drawer(
-        child: SingleChildScrollView(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            buildHeader(context),
-            buildMenuItems(context),
-          ],
-        )),
-      );
-
-  Widget buildHeader(BuildContext context) => Container(
-    color: Colors.blue.shade700,
-    padding: EdgeInsets.only(
-      top: 24 + MediaQuery.of(context).padding.top,
-      bottom: 24,
-    ),
-    child: Column(
-      children: const [
-        CircleAvatar(radius: 52,),
-        Text('Orienteering APP', style: TextStyle(fontSize: 28, color: Colors.white),),
-        Text('Races Results', style: TextStyle(fontSize: 15, color: Colors.white),)
-      ],
-    ),
-  );
-  
-  Widget buildMenuItems(BuildContext context) => Container(
-    padding: const EdgeInsets.all(24),
-    child: Wrap(
-      runSpacing: 16,
-      children: [
-        /* ListTile(
-          leading: const Icon(Icons.home_outlined),
-          title: const Text('Home'),
-          onTap: () {},
-        ), */
-        ListTile(
-          leading: const Icon(Icons.run_circle_outlined),
-          title: const Text('List Races'),
-          onTap: () {
-            Navigator.pop(context); // close the Draw
-            Navigator.pop(context); // return to Home page
-          },
-        ),
-        const Divider(color: Colors.black54,),
-        ListTile(
-          leading: const Icon(Icons.people_alt_sharp),
-          title: const Text('About us'),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const about_us(),
-              ),
-            );
-          },
-        ),
-      ],
-    ),
-  );
 }
