61c8b3cb5d3210c0e2ea0722723476c2e5713b0e
GecheleStefano <67332771+GecheleStefano@users.noreply.github.com>
2022-05-27 18:59:50 +0200

    Update about_us.dart

--- Golduck/flutter_orientiring/lib/about_us.dart
+++ Golduck/flutter_orientiring/lib/about_us.dart
@@ -28,26 +28,30 @@ class about_us_state extends State<about_us> {
       ),
       body: Column(
         children: [
-          Image.network(
-            'https://portalebambini.it/wp-content/uploads/2015/10/orienteering1.jpg',
-            fit: BoxFit.fitWidth,
+          Container(
+            constraints: const BoxConstraints(
+                minHeight: 100, minWidth: double.infinity, maxHeight: 400),
+            child: Image.network(
+              'https://portalebambini.it/wp-content/uploads/2015/10/orienteering1.jpg',
+              fit: BoxFit.fitWidth,
+            ),
           ),
-          Text('ORIENTEERING',
+          const Text('ORIENTEERING',
               style: TextStyle(height: 2, fontSize: 30),
               textAlign: TextAlign.center),
-          Text(
+          const Text(
             'Stefano Gechele',
             textAlign: TextAlign.center,
           ),
-          Text('Giorgio Chirico', textAlign: TextAlign.center),
-          Text('Davide Revrena', textAlign: TextAlign.center),
-          Text('Fabio Assolari', textAlign: TextAlign.center),
+          const Text('Giorgio Chirico', textAlign: TextAlign.center),
+          const Text('Davide Revrena', textAlign: TextAlign.center),
+          const Text('Fabio Assolari', textAlign: TextAlign.center),
         ],
       ),
 
       // floating botttom 'home'
       floatingActionButton: FloatingActionButton(
-        hoverColor: Color.fromARGB(121, 133, 133, 133),
+        hoverColor: const Color.fromARGB(121, 133, 133, 133),
         hoverElevation: 50,
         tooltip: 'Return to Home',
         elevation: 12,
