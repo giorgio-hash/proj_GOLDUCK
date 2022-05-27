dee2d8dccb9569df11333ffd3267f2c504d83027
fassolari1 <fabioassolari1@gmail.com>
2022-05-27 18:56:51 +0200

    update main

--- Golduck/flutter_orientiring/lib/main.dart
+++ Golduck/flutter_orientiring/lib/main.dart
@@ -164,66 +164,39 @@ class NavigationDrawer extends StatelessWidget {
       );
 
   Widget buildHeader(BuildContext context) => Container(
-        color: Colors.blue.shade700,
-        padding: EdgeInsets.only(
-          top: 24 + MediaQuery.of(context).padding.top,
-          bottom: 24,
-        ),
-        child: Column(
-          children: const [
-            CircleAvatar(
-              radius:
-                  52, /* foregroundImage: NetworkImage('https://www.rete8.it/wp-content/uploads/2018/09/orienteering1-777x437.jpg') */
-            ),
-            Text(
-              'Orienteering APP',
-              style: TextStyle(fontSize: 28, color: Colors.white),
-            ),
-            Text(
-              'Races Results',
-              style: TextStyle(fontSize: 15, color: Colors.white),
-            )
-          ],
-        ),
-      );
-
+    color: Colors.blue.shade700,
+    padding: EdgeInsets.only(
+      top: 24 + MediaQuery.of(context).padding.top,
+      bottom: 24,
+    ),
+    child: Column(
+      children: const [
+        CircleAvatar(radius: 52,
+        /* foregroundImage: NetworkImage('https://www.rete8.it/wp-content/uploads/2018/09/orienteering1-777x437.jpg') */),
+        Text('Orienteering APP', style: TextStyle(fontSize: 28, color: Colors.white),),
+        Text('Races Results', style: TextStyle(fontSize: 15, color: Colors.white),)
+      ],
+    ),
+  );
+  
   Widget buildMenuItems(BuildContext context) => Container(
-        padding: const EdgeInsets.all(24),
-        child: Wrap(
-          runSpacing: 16,
-          children: [
-            /* ListTile(
+    padding: const EdgeInsets.all(24),
+    child: Wrap(
+      runSpacing: 16,
+      children: [
+        /* ListTile(
           leading: const Icon(Icons.home_outlined),
           title: const Text('Home'),
           onTap: () {},
         ), */
-<<<<<<< HEAD
-            /* ListTile(
+        ListTile(
           leading: const Icon(Icons.run_circle_outlined),
           title: const Text('List Races'),
           onTap: () {
             Navigator.pop(context); // close the Draw
+            Navigator.pop(context); // close the menu
           },
-        ), */
-            const Divider(
-              color: Colors.black54,
-            ),
-            ListTile(
-              leading: const Icon(Icons.people_alt_sharp),
-              title: const Text('About us'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const about_us(),
-                  ),
-                );
-              },
-            ),
-          ],
         ),
-      );
-=======
         const Divider(color: Colors.black54,),
         ListTile(
           leading: const Icon(Icons.people_alt_sharp),
@@ -240,5 +213,4 @@ class NavigationDrawer extends StatelessWidget {
       ],
     ),
   );
->>>>>>> 534d7db578a138c3e437beafa3a7fced6ef92cb9
 }
