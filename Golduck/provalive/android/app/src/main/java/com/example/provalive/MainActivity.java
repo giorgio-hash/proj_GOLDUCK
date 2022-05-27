17d80a260595890326780d3c256132beb37ca018
GecheleStefano <67332771+GecheleStefano@users.noreply.github.com>
2022-05-27 19:38:40 +0200

    Delete Golduck/provalive directory

--- Golduck/provalive/android/app/src/main/java/com/example/provalive/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.provalive;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- Golduck/provalive/lib/globals.dart
+++
@@ -1,2 +0,0 @@
-// TODO: inserire proprio indirizzo AWS tipo: https://....us-east-1.amazonaws.com
-const apiUrl = 'https://eglle95ua9.execute-api.us-east-1.amazonaws.com';
